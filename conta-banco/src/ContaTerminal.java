5ec7453303d68f2332fcfd626a0ccfe586d9cd04
KellyCrisLemos <kellylemos234@gmail.com>
2024-06-07 11:19:48 -0300

    tete

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,28 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    /**
+     * @param args
+     */
+
+     public static void main(String[] args) {
+        System.out.println("Olá!");
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.print("MARIO ANDRADE");
+            String nome = scanner.nextLine();
+
+            System.out.print("1021");
+            int ContaCorrente = scanner.nextInt();
+
+            System.out.print("067");
+            int agencia = scanner.nextInt();
+
+            System.out.print("237,48");
+            int saldo = scanner.nextInt();
+
+            System.out.println("Olá" + nome + "obrigado por criar uma conta em nosso banco, sua agência é" + agencia + "conta" + ContaCorrente + "e seu saldo" + saldo + "já está disponível para saque.");
+        }
     }
+        
+    
 }
